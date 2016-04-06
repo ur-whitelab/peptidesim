9a488c8931633cb1a0e34d1def41068b68fbe991
Andrew White <white.d.andrew@gmail.com>
2016-04-06 01:12:26 -0400

    Got Dox to work, all tests pass

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name = 'peptidesim',
       url = 'http://thewhitelab.org/Software',
       license = 'GPL3',
       packages = ['peptidesim'],
-      install_requires=['PeptideBuilder==1.0.1', 'GromacsWrapper>= 0.4'],
+      install_requires=['PeptideBuilder==1.0.1', 'GromacsWrapper== 0.4.0'],
       dependency_links=['https://github.com/mtien/PeptideBuilder/archive/master.zip#egg=PeptideBuilder-1.0.1', 'https://github.com/Becksteinlab/GromacsWrapper/archive/develop.zip#egg=GromacsWrapper-0.4.1'],
       test_suite='tests',
       zip_safe=True
