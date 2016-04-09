4dfb3ddfefd1205823e965d5721070067b8cfd1e
Han Lin <linhan@dhcp-10-5-2-149.wireless.rochester.edu>
2016-04-09 16:34:01 -0400

    Wrote a test to 1) Check if the images exist. 2) Chcek the existance of the images path. 3) Check the timestamps are correct or not.

--- tests/peptidesim_test.py
+++ tests/peptidesim_test.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
 import peptidesim
+import os.path, time
 
 class TestPeptideSim(TestCase):
     def test_sanity(self):
         self.assertTrue(True)
+        
+    def test_analysis_returns_images(self):
+
+        prea_time = time.ctime()
+        ret = p.analysis()
+        
+        self.assertTrue(len(ret) > 0)
+        
+        #check if the images exist
+        oldest = time.ctime()
+        for path in ret:
+            self.assertTrue(os.path.exists(path))
+            #get oldest
+            oldest = min(oldest, time.ctime(os.path.getctime(path)))
+        #check that the oldest image was created after analysis call
+        self.assertTrue(prea_time < oldest)
