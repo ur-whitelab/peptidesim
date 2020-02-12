0fe3910c8e2c6a880138d48ddbdd268305b739e5
Dilnoza Amirkulova <dilnoza92@users.noreply.github.com>
2020-02-12 11:02:31 -0500

    updated the scripts that run abeta simulation and is python3 compatible

---
+++ inputs/running_abeta_scripts/part_1.py
@@ -0,0 +1,263 @@
+from peptidesim import PeptideSim
+import textwrap, sys, re, os
+import sys
+import gromacs
+import dill as pickle
+from shutil import copyfile, move
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+seq = 'AEEE'#sys.argv[1]
+name = 'playing' #sys.argv[2]
+debug = False
+pickle_name = name + '.pickle'
+MPI_NP = 2
+peptide_copies=1#int(sys.argv[3])
+
+#try to reload                                                                                
+if(os.path.exists(pickle_name)):
+    print('loading restart')
+    with open(pickle_name, 'r') as f:
+        ps = pickle.load(f)
+else:
+    ps = PeptideSim(name, [seq], [peptide_copies], job_name='{}'.format(name))
+    ps.mdrun_driver='gmx_mpi'
+    ps.forcefield='amber99sb'
+    ps.water='tip4p'
+    ps.peptide_density = 0.008#mg/ml 
+    ps.ion_concentration=0.001#10mM 
+    ps.initialize()
+    with open(ps.pickle_name, 'w') as f:
+        pickle.dump(ps, file=f)
+
+
+file00=ps.pdb_file
+cwd=os.getcwd()
+#gromacs.pdb2gmx(f=file00,o="file_pdb_output.pdb",water=ps.water,ff=ps.forcefield)
+#file00="{}/{}".format(cwd,"file_pdb_output.pdb")
+#print(file00,"file_pdb_output.pdb", cwd)
+def total_aa(file1,output_file):
+    print(ps.sims[-1].location)
+    output=open(output_file,'w')
+    with open(file1, "r") as f:
+        lines=f.readlines()
+        i=4
+     
+        for line in lines[:4]:
+            output.write(line)
+            
+     
+        for line in lines[4:]:
+            old_line=line
+            line=line.split()
+            i=i+1
+            if(len(line)>1 and line[3]=='SOL'):
+                
+                output.write('TER final\n')
+                output.write('ENDMDL final\n')      
+                output.close()
+                break
+            #i=i+1
+            output.write(old_line)
+        last_line=[]
+        print(lines[i-1],lines[i-2], lines[i-3])
+        if (lines[i-1].startswith('END')==True):
+                
+            last_line=lines[i-3].strip()
+            print("ends with END")
+        else:
+            last_line=lines[i-1].strip()
+            print("doesnt end with END")
+
+        last_line=last_line.split()
+
+        output.close()
+        if (last_line[4].isdigit()==True):
+            return int(last_line[1]),int(last_line[4]),output_file
+        else:
+            return int(last_line[1]),int(last_line[5]),output_file
+#print total_aa(file00,'template.pdb')
+
+def number_all_atoms_chains():
+    if (os.path.isdir("{}/".format(os.getcwd)+"data")==True):
+        template_file="{}/".format(os.getcwd)+"data/template.pdb"
+        #template_file="{}/".format(os.getcwd)+"data/template.pdb"
+        with open(template_file, 'r') as f:
+            lines=f.readlines()
+            lines=lines[-3].strip()
+            lines=lines.split()
+            return lines[1],last_lines[5]
+total_no_atoms,number_chains,file0=total_aa(file00,'template.pdb')
+atoms_in_chain=int(int(total_no_atoms)/int(peptide_copies))
+
+
+with open(file0,'r') as file_read:
+    file_data=file_read.read()
+file_data=file_data.replace('HIS','HIE')
+with open(file0,'w') as file_write:
+    file_write.write(file_data)
+
+        
+def pdbfile_generator_w_chain_id(number_of_chains,atoms_in_chain,first_atom_index,output_pdbfile,input_pdbfile):
+    '''a funtion that takes an old pdbfile that has all hydrogens but without unique chain IDs 
+            and without terminii of chains indicated and generates a new pdbfile with uniqe chain id's
+            and terminii indicated. 
+       
+            var:
+       
+            number_of_chains-an int that holds copies of the same chain
+            atoms_in_chain-an int that holds number of atoms in each chain
+            first_atom_index-an index of the line containing first atom 
+            in the old pdbfile (line indexing starts at 0)
+           output_pdbfile-a string containing the name of the output pdbfile
+           input_pdbfile-a string containing the input pdbfile
+        
+           returns:
+        
+           newp pdb file'''
+    from string import ascii_uppercase
+    print(input_pdbfile,number_of_chains,atoms_in_chain)
+    with open(input_pdbfile, 'r') as f:
+        lines=f.readlines()
+        beginning=lines[:first_atom_index]                  #saves the first useless lines that don't contain conf info
+        lines=lines[first_atom_index:]                      #gets rid of those lines from readlines
+        first_line=lines[0].strip()
+        first_line=first_line.split()
+        with open(output_pdbfile,'w') as f:
+            for index in np.arange(len(beginning)):         #iterates through useless lines
+                f.write("{}\n".format(beginning[index].strip())) #writes the useless lines into the new file   
+            for i in np.arange(number_of_chains):           #iterates through the copies of chains  
+                for j in np.arange(atoms_in_chain):         #itarates through the atoms in the chain
+                    #print(i*(atoms_in_chain+1)+j,i*(atoms_in_chain)+j, len(lines), i,j)
+                    a=lines[i*(atoms_in_chain)+i+j]             #reads the old pdbfile info pertaining to the atoms of interest  
+                    a=list(a)                               #converts the string into a list of characters 
+                    if (len(a)>=21):
+                        a[21]=ascii_uppercase[i]                #replaces the repetittive chain ID with unique chain ID chosen from 
+                        
+                    a="".join(a)
+                            #puts the list of characters back to a string that contains line info
+                    f.write('{}'.format(a))                 #writes the line into the new pdbfile
+
+                    #if(int(i*(atoms_in_chain-1)+i+j)==int((atoms_in_chain-1)*i+atoms_in_chain-1+i) and peptide_copies!=1): #checks whether the atoms is at the end of the chain    
+                    if(j==int(atoms_in_chain-1)):   #print int(i*atoms_in_chain+j),int((atoms_in_chain*i+atoms_in_chain-1))
+                    #    print "wrote TER"
+                        f.write('TER second\n')                    #puts ter at the end of each chain
+                if (peptide_copies==i+1):
+                    f.write('ENDMDL second\n')                             #puts finish touches
+                    f.close()                                       #done
+    return output_pdbfile 
+
+file3='template.pdb'
+new_pdb_file=pdbfile_generator_w_chain_id(int(peptide_copies),int(atoms_in_chain),5,'new_pdb.pdb',file0)
+
+directory='data'
+if not os.path.exists(directory):
+    os.mkdir(directory)
+template_pdb=os.path.join(directory,file3)
+    
+camshift_src='/home/damirkul/simulation_software/simulations/AAA/data/camshift.db'
+gromacs_a03_mdb_src='/home/damirkul/simulation_software/simulations/AAA/data/a03_gromacs.mdb'
+h_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid_21_30/data/Hshifts.dat'
+ha_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid_21_30/data/HAshifts.dat'
+#c_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid/data_old/Cshifts.dat'     
+#ca_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid/data_old/CAshifts.dat'   
+#cb_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid/data_old/CBshifts.dat'   
+#n_src='/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid/data_old/Nshifts.dat'     
+gromacs_a03_mdb_dst=os.path.join(directory,'a03_gromacs.mdb')
+camshift_dst=os.path.join(directory,'camshift.db')
+template_pdb=os.path.join(directory,file3)
+h_dst=os.path.join(directory,'Hshifts.dat')
+ha_dst=os.path.join(directory,'HAshifts.dat')
+#c_dst=os.path.join(directory,'Cshifts.dat')                                                  
+#ca_dst=os.path.join(directory,'CAshifts.dat')                                                
+#cb_dst=os.path.join(directory,'CBshifts.dat')                                                
+#n_dst=os.path.join(directory,'Nshifts.dat')                                                  
+copyfile(h_src,h_dst)
+copyfile(ha_src,ha_dst)
+#copyfile(c_src,c_dst)                                                                        
+#copyfile(ca_src,ca_dst)                                                                      
+#copyfile(cb_src,cb_dst)                                                                      
+#copyfile(n_src,n_dst)                                                                     
+move(new_pdb_file,template_pdb)
+copyfile(camshift_src,camshift_dst)
+copyfile(gromacs_a03_mdb_src, gromacs_a03_mdb_dst)
+
+center=''
+
+        
+def data_folder(number_amino_acids,name,copies_chains):
+    ''' a function that takes total number of amino acids in the pdbfile,name of the chemical shifts file and how many copies of a peptide are present and puts chemical shifta file into the data folder        
+    Var:
+       number_amino_acids-total number of amino acids in the pdbfile 
+       name-name of the chemical shifts file                                                   
+       copies_chains-the number of peptide copies
+       '''
+    new_file=os.path.join(directory,name)                                  #creates a chemical shift file in data folder
+    if (name!='HAshifts.dat' and name!='Hshifts.dat'):                       #checks whether the exp chem shifts exist, for now only H shifts are available
+
+        with open(new_file,'w') as f:                                     #creates a new file to copy over the chemical shufts
+            beg_end_chains=[]                                              #amino acid residue indeces for beginning and end of each chain of peptides                                                                  
+            number_chains=number_amino_acids/copies_chains                 #number of amino acids in one chain                                                                                                                    
+            for i in range(copies_chains):                                 #iterates throug a number of copies
+                beg_end_chains.append(i*number_chains+1)                   #adds the begining amino acid index of the chain i, first index starts at 1
+                beg_end_chains.append(i*number_chains+number_chains)       #adds the amino acid ending index of the chain
+                for j in range(number_chains):                             #iterates through the amino acid indeces in the chain 
+                    j=j+1                                                  #amino acid index also starts at 1
+                    if (j in beg_end_chains):                              #checks whether the amino acid index is at the beginning or ending of the chain                        
+                        f.write('#{} 1\n'.format(number_chains*i+j))       #puts a hash sign for beginning and amino acid indeces in the chain and 1 for lack of exp chem shift
+                    else:
+                        f.write('{} 1\n'.format(number_chains*i+j))        #puts 1 for lack of exp chem shifts
+    else:
+        with open(new_file,'w') as f:
+            beg_end_chains=[]                                              #amino acid residue indeces for beginning and end of each chain of peptides                                                                                                         
+            number_chains=number_amino_acids/copies_chains                 #number of amino acids in one chain                                                                                                                    
+            chemical_shifts=np.genfromtxt('/scratch/damirkul/pep_simulations/sequences_amyloid/amyloid_21_30/data/{}'.format(name),comments='!#')
+#            print chemical_shifts
+            for i in range(copies_chains):
+                beg_end_chains.append(i*number_chains+1)                   #adds the begining amino acid index of the chain i, first index starts at 1
+                beg_end_chains.append(i*number_chains+number_chains)       #adds the amino acid ending index of the chain
+                #print beg_end_chains
+                for j in range(number_chains):                             #iterates through the amino acid indeces in the chain
+                    j=j+1                                                  #amino acid index also starts at 1
+                    if (j in beg_end_chains):                              #checks whether the amino acid index is at the beginning or ending of the chain
+                        #print i,j,number_chains*i+j,chemical_shifts[j-1][1],'a' 
+                        f.write('#{} {}\n'.format(number_chains*i+j,chemical_shifts[j-1][1]))       #puts a hash sign for beginning and amino acid indeces in the chain
+                    else:
+                        #print i,j,number_chains*i+j,chemical_shifts[j-1][1]
+                        f.write('{} {}\n'.format(number_chains*i+j,chemical_shifts[j-1][1]))       #copies the chem shifts from file with chemical shifts for the correspoding amino acid
+                        
+filenames=['Cshifts.dat','CAshifts.dat','HAshifts.dat','Hshifts.dat','CBshifts.dat', 'Nshifts.dat']
+for i in filenames:
+    data_folder(number_chains,i,peptide_copies)
+ps.add_file(directory)
+with open(ps.pickle_name, 'w') as f:
+    pickle.dump(ps, file=f)
+
+print(ps.pickle_name, 'picklename1')
+with open(ps.pickle_name, 'w') as f:
+    pickle.dump(ps, file=f)
+
+ps.run(mdpfile='peptidesim_emin.mdp', tag='init_emin', mdp_kwargs={'nsteps': 8*10**2,'rcoulomb':1}, mpi_np=MPI_NP)
+
+#print ps.pickle_name, 'picklename2'
+with open(ps.pickle_name, 'w') as f:
+    pickle.dump(ps, file=f)
+ps.run(mdpfile='peptidesim_anneal.mdp',tag='annealing',mdp_kwargs={'nsteps':int(5* 5*10**2)},run_kwargs={'cpt':5},mpi_np=MPI_NP)#change the time step to 2 ns
+#ps.run(mdpfile='peptidesim_anneal.mdp',tag='annealing',mdp_kwargs={'nsteps':int(2000* 5*10**2)},run_kwargs={'cpt':5},mpi_np=MPI_NP)#change the time step to 2 ns
+
+
+print(ps.pickle_name, 'picklename3')
+with open(ps.pickle_name, 'w') as f:
+    pickle.dump(ps, file=f)
+
+ps.run(mdpfile='peptidesim_npt.mdp', tag='equil_npt', mdp_kwargs={'nsteps': int(5 * 5*10**2),'ref_t':278},run_kwargs={'cpt':5},mpi_np=MPI_NP)
+#ps.run(mdpfile='peptidesim_npt.mdp', tag='equil_npt', mdp_kwargs={'nsteps': int(2000 * 5*10**2),'ref_t':278},run_kwargs={'cpt':5},mpi_np=MPI_NP)
+
+print(ps.pickle_name, 'picklename3')
+with open(ps.pickle_name, 'w') as f:
+    pickle.dump(ps, file=f)
+            
+
+
---
+++ inputs/running_abeta_scripts/part_2.py
@@ -0,0 +1,148 @@
+from peptidesim import PeptideSim
+import textwrap, sys, re, os
+import sys
+import dill as pickle
+from shutil import copyfile, move
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+name = sys.argv[2]
+debug = False
+pickle_name = name + '.pickle'
+MPI_NP =4
+peptide_copies=int(sys.argv[3])
+replicas=2
+#try to reload                                                            
+total_no_atoms=0                 #init
+number_chains=0                  #init
+ps=3                             #initialize
+pte_tune_time = 0.05             #in ns will run at least 4 time, total time will be 40 ns                     
+pt_wte_eds_time_ns = 0.05         #10 #in ns will run at least 4 times,resulting in 40 ns sim                     
+final_eds_time_ns = 0.05          #40 in ns will run once resulting in 40ns simulation                            
+replica_eff_threshold=0.01      #minimum thereshold for both pt-wte and eds+pt-wte                              
+cpt_time=5 #60                   #checkpoint file output interval in mins 
+
+remd_exchange=10 #250
+total_no_atoms,number_chains=0,0
+
+if(os.path.exists(pickle_name)):
+    print('loading restart')
+    with open(pickle_name, 'rb') as f:
+        ps = pickle.load(f)
+        #ps.pickle_name=pickle_name
+        print(os.getcwd())
+        #ps.rel_dir_name='.'
+        def number_all_atoms_chains():
+            if (os.path.isdir("{}/data/".format(os.getcwd()))==True):
+                template_file="{}/data/template.pdb".format(os.getcwd())
+                with open(template_file, 'r') as f:
+                    lines=f.readlines()
+                    lines=lines[-3].strip()
+                    lines=lines.split()
+                    print(lines)
+                    return int(lines[1]),int(lines[5])
+            else:
+                print("there is no data directory")
+        total_no_atoms,number_chains=number_all_atoms_chains()
+
+old_gro_file=ps.gro_file
+mdp_kwargs={'nsteps':int(400* 5*10**2)}
+replica_kwargs_0 = [mdp_kwargs.copy() for _ in range(4)]
+for k in range(4):
+    replica_kwargs_0[k]['ref_t'] = 278
+    
+#ps.run(mdp_kwargs=replica_kwargs, run_kwargs={'cpt':cpt_time, 'cpnum':'yes',  'replex':200, 'multi':4,'plumed':'plumed_restraint.dat'},mdpfile='peptidesim_nvt.mdp', tag='restraint',mpi_np=4)
+ps.gro_file=old_gro_file
+
+eds_period=10
+remd_exchange_period=10
+def get_replex_e(ps, replica_number):
+    with open(ps.sims[-1].location + '/' + ps.sims[-1].metadata['md-log']) as f:
+        p1 = re.compile('Repl  average probabilities:')       
+        p2 = re.compile('Repl\s*' + ''.join(['([0-9\.]+\s*)' for _ in range(replica_number - 1)]) + '$')
+        ready = False
+        #answer=-1
+        for line in f:
+            if not ready and p1.findall(line):
+                ready = True
+            elif ready:
+                match = p2.match(line)
+                if match:
+                    return [float(s) for s in match.groups()]
+    
+                
+
+for i in ps.sims:
+    print(i.name)
+
+pte_result = ps.pte_replica(mpi_np=MPI_NP, max_tries=2,min_iters=1,
+                              mdp_kwargs={'nsteps':int(pte_tune_time* 5*10**5)}, replicas=replicas,hills_file_location=os.getcwd(),run_kwargs={'cpt':cpt_time},
+                              hot=279,hill_height=1,sigma=500,bias_factor=20,eff_threshold=replica_eff_threshold,cold=278,exchange_period=2)
+with open(ps.pickle_name, 'wb') as f:
+    pickle.dump(ps, file=f)
+
+pte_plumed_script=pte_result['plumed']
+replica_temps=pte_result['temperatures']
+temps = ','.join(str(e) for e in replica_temps)
+kwargs = [ {'ref_t': ti} for ti in replica_temps]
+print(total_no_atoms,number_chains,eds_period,temps)
+#now reload PTE_WTE hills file and run eds with cs2backbone to generate the eds parameters with multiple replicas
+plumed_input0=textwrap.dedent(
+    '''
+
+    peptide: GROUP ATOMS=1-{}                                                                                      
+    WHOLEMOLECULES ENTITY0=peptide                                                                                 
+    cs: CS2BACKBONE ATOMS=peptide DATADIR=data                                                          
+
+    #bias the simalation with EDS and chem chifts until one gets good eds convergence
+     
+    PRINT ARG=(cs\.cb_.*),(cs\.ca_.*),(cs\.hn_.*),(cs\.ha_.*),(cs\.nh_.*),(cs\.co_.*) FILE=WT_PTE_CS_shifts STRIDE=200                     
+    #exp_mean: MATHEVAL ARG=cs.exphn_1,cs.exphn_2,cs.exphn_3,cs.exphn_4,cs.exphn_7,cs.exphn_8,cs.expha_1,cs.expha_2,cs.expha_3,cs.expha_5 VAR=x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 FUNC=(x1+x2+x3+x4+x5+x6+x7+x8+x9+x10)/10 PERIODIC=NO
+    #calc_mean: MATHEVAL ARG=cs.hn_1,cs.hn_2,cs.hn_3,cs.hn_4,cs.hn_7,cs.hn_8,cs.ha_1,cs.ha_2,cs.ha_3,cs.ha_5 VAR=x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 FUNC=(x1+x2+x3+x4+x5+x6+x7+x8+x9+x10)/10 PERIODIC=NO
+
+
+    eds: EDS ARG=cs.hn-0-5,cs.hn-0-2,cs.hn-0-3,cs.hn-0-4,cs.hn-0-7,cs.hn-0-8,cs.ha-0-4,cs.ha-0-2,cs.ha-0-3,cs.ha-0-6 CENTER_ARG=cs.exphn-0-5,cs.exphn-0-2,cs.exphn-0-3,cs.exphn-0-4,cs.exphn-0-7,cs.exphn-0-8,cs.expha-0-4,cs.expha-0-2,cs.expha-0-3,cs.expha-0-6 MULTI_PROP=0.4 RANGE=0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9 PERIOD={} TEMP=@replicas:{} OUT_RESTART={}/eds_bias_restart_correct2.dat
+
+    
+    ENDPLUMED
+    '''.format(total_no_atoms,eds_period,'{'+temps+'}',os.getcwd()))
+plumed_input=pte_plumed_script+plumed_input0
+with open('plumed_eds_conver_pt_wte_metad.dat', 'w') as f:
+    f.write(plumed_input)
+ps.add_file('plumed_eds_conver_pt_wte_metad.dat')
+time_ns=.40
+replex_eff = 0
+for kw in kwargs:
+    kw['nsteps']=  int(pt_wte_eds_time_ns * 5 * 10 ** 5)
+
+
+
+ps.run(mdpfile='peptidesim_nvt.mdp', tag='nvt_conver_eds_{}'.format(2),  mdp_kwargs=kwargs,run_kwargs={'plumed':'plumed_eds_conver_pt_wte_metad.dat', 'replex': remd_exchange, 'cpt':cpt_time},mpi_np=MPI_NP)
+with open(ps.pickle_name, 'wb') as f:
+    pickle.dump(ps, file=f)
+    
+eds_conver_ptwte_folder=ps.sims[-1].location
+print(eds_conver_ptwte_folder)     
+colvar_file='{}/restart_pt_wte.0.dat'.format(os.path.abspath(eds_conver_ptwte_folder))
+
+plumed_input=textwrap.dedent(
+'''
+    peptide: GROUP ATOMS=1-{}                                                                                      
+    WHOLEMOLECULES ENTITY0=peptide                                                                                 
+    cs: CS2BACKBONE ATOMS=peptide DATADIR=data 
+
+    #bias the simalation with EDS and chem chifts until one gets good eds convergence
+    
+
+eds: EDS ARG=cs.hn-0-5,cs.hn-0-2,cs.hn-0-3,cs.hn-0-4,cs.hn-0-7,cs.hn-0-8,cs.ha-0-4,cs.ha-0-2,cs.ha-0-3,cs.ha-0-6 CENTER_ARG=cs.exphn-0-5,cs.exphn-0-2,cs.exphn-0-3,cs.exphn-0-4,cs.exphn-0-7,cs.exphn-0-8,cs.expha-0-4,cs.expha-0-2,cs.expha-0-3,cs.expha-0-6 MULTI_PROP=0.4 RANGE=0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1 PERIOD=2 TEMP=@replicas:{} OUT_RESTART={}/{}
+
+    ENDPLUMED'''.format(total_no_atoms,'{'+temps+'}',os.getcwd(),'OUTPUT_COLVAR_EDS'))
+
+
+with open('plumed_eds_colvars_new.dat', 'w') as f:
+    f.write(plumed_input)
+ps.add_file('plumed_eds_colvars_new.dat')
+#ps.req_files.append('plumed_eds_colvars_new.dat')
