3c137b07d61a5ff05e8d1908271212605a756cdf
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-06-29 14:22:15 -0400

    Added replex eff calculation and fixed plumed inputs

--- inputs/abeta_sims/part_2.py
+++ inputs/abeta_sims/part_2.py
@@ -45,14 +45,26 @@ replica_kwargs_0 = [mdp_kwargs.copy() for _ in range(4)]
 for k in range(4):
     replica_kwargs_0[k]['ref_t'] = 278
 
-# ps.run(mdp_kwargs=replica_kwargs,
-#        run_kwargs={'cpt':cpt_time, 'cpnum':'yes',
-#                    'replex':200, 'multi':4,'plumed':'plumed_restraint.dat'},
-#        mdpfile='peptidesim_nvt.mdp', tag='restraint',mpi_np=4)
 ps.gro_file = old_gro_file
 
 eds_period = 10
 remd_exchange_period = 10
+
+# To calculate replica exchange efficiency from md.log
+def get_replex_e(ps, replica_number):
+    with open(ps.sims[-1].location + '/' + ps.sims[-1].metadata['md-log']) as f:
+        p1 = re.compile('Repl  average probabilities:')
+        p2 = re.compile(
+        r'Repl\s*' + ''.join([r'([0-9\.]+\s*)' for _ in range(replica_number - 1)]) + '$')
+        ready = False
+        # answer=-1
+        for line in f:
+            if not ready and p1.findall(line):
+                ready = True
+            elif ready:
+                match = p2.match(line)
+                if match:
+                    return [float(s) for s in match.groups()]
 
 
 for i in ps.sims:
@@ -85,21 +97,8 @@ kwargs = [{'ref_t': ti} for ti in replica_temps]
 # now reload PTE_WTE hills file and run eds with cs2backbone to generate
 # the eds parameters with multiple replicas
 
-iteration_number = 0
-list_of_indices = [0]
-index_of_eds_file = 0
-for any_file in ps.sims:
-    if any_file.name.startswith('nvt_conver_eds_2'):
-        for file_name in ps.file_list:
-            if 'number' in file_name:
-                f2 = open(file_name, 'r')
-                iteration_number = int(f2.readlines()[0])
-            list_of_indices.append(iteration_number)
-    index_of_eds_file = max(list_of_indices)
-    with open('number_{}'.format(index_of_eds_file + 1), 'w') as f:
-        f.write('{}'.format(index_of_eds_file + 1))
-        f.close()
-    ps.add_file('number_{}'.format(index_of_eds_file + 1))
+max_eds_iterations = 2
+index_of_eds_file = max_eds_iterations - 1
 
 # if the eds is being called for the first time then the eds file index zero,
 # hence we do not need call IN_Restart argument in EDS line
@@ -118,7 +117,7 @@ plumed_input0 = textwrap.dedent(
     #exp_mean: MATHEVAL ARG=cs.exphn_1,cs.exphn_2,cs.exphn_3,cs.exphn_4,cs.exphn_7,cs.exphn_8,cs.expha_1,cs.expha_2,cs.expha_3,cs.expha_5 VAR=x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 FUNC=(x1+x2+x3+x4+x5+x6+x7+x8+x9+x10)/10 PERIODIC=NO
     #calc_mean: MATHEVAL ARG=cs.hn_1,cs.hn_2,cs.hn_3,cs.hn_4,cs.hn_7,cs.hn_8,cs.ha_1,cs.ha_2,cs.ha_3,cs.ha_5 VAR=x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 FUNC=(x1+x2+x3+x4+x5+x6+x7+x8+x9+x10)/10 PERIODIC=NO
 
-    eds: EDS ARG=cs.hn-0-5,cs.hn-0-2,cs.hn-0-3,cs.hn-0-4,cs.hn-0-7,cs.hn-0-8,cs.ha-0-4,cs.ha-0-2,cs.ha-0-3,cs.ha-0-6 CENTER_ARG=cs.exphn-0-5,cs.exphn-0-2,cs.exphn-0-3,cs.exphn-0-4,cs.exphn-0-7,cs.exphn-0-8,cs.expha-0-4,cs.expha-0-2,cs.expha-0-3,cs.expha-0-6 MULTI_PROP=0.4 RANGE=0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9 PERIOD={} TEMP=@replicas:{} OUT_RESTART={}/eds_bias_restart_correct0.dat
+    eds: EDS ARG=cs.hn-0-5,cs.hn-0-2,cs.hn-0-3,cs.hn-0-4,cs.hn-0-7,cs.hn-0-8,cs.ha-0-4,cs.ha-0-2,cs.ha-0-3,cs.ha-0-6 CENTER_ARG=cs.exphn-0-5,cs.exphn-0-2,cs.exphn-0-3,cs.exphn-0-4,cs.exphn-0-7,cs.exphn-0-8,cs.expha-0-4,cs.expha-0-2,cs.expha-0-3,cs.expha-0-6 MULTI_PROP=0.4 RANGE=0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9 PERIOD={} TEMP=@replicas:{} OUT_RESTART={}/eds_bias_restart0.dat
 
     ENDPLUMED
     '''.format(total_no_atoms, eds_period, '{' + temps + '}', os.getcwd()))
@@ -129,7 +128,7 @@ plumed_input0 = textwrap.dedent(
 # We also need create a list of eds files that need to be read during restart
 eds_file_string = ''
 for replica in range(replicas):
-    eds_file_string += '{}/eds_bias_restart_correct{}{}.dat,'.format(
+    eds_file_string += '{}/eds_bias_restart{}.{}.dat,'.format(
         os.getcwd(), index_of_eds_file, replica)
 # remove the comma after the last file name
 eds_file_string = eds_file_string[:-1]
@@ -149,7 +148,7 @@ plumed_input1 = textwrap.dedent(
     eds: EDS ARG=cs.hn-0-5,cs.hn-0-2,cs.hn-0-3,cs.hn-0-4,cs.hn-0-7,cs.hn-0-8,cs.ha-0-4,cs.ha-0-2,cs.ha-0-3,cs.ha-0-6 CENTER_ARG=cs.exphn-0-5,cs.exphn-0-2,cs.exphn-0-3,cs.exphn-0-4,cs.exphn-0-7,cs.exphn-0-8,cs.expha-0-4,cs.expha-0-2,cs.expha-0-3,cs.expha-0-6 MULTI_PROP=0.4 RANGE=0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9 PERIOD={} TEMP=@replicas:{} OUT_RESTART={}/eds_bias_restart_correct{}.dat IN_RESTART=@replicas:{}
 
     ENDPLUMED
-    '''.format(total_no_atoms, eds_period, '{' + temps + '}', os.getcwd(), index_of_eds_file + 1, '{' + eds_file_string + '}'))
+    '''.format(total_no_atoms, eds_period, '{' + temps + '}', os.getcwd(), index_of_eds_file, '{' + eds_file_string + '}'))
 plumed_file = 'plumed_eds_conver_pt_wte' + \
     '{}'.format(index_of_eds_file) + '.dat'
 if index_of_eds_file == 0:
