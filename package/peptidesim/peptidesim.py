bd130e769e2434d4f1fd24041c8b0ee1fbaff618
Andrew White <white.d.andrew@gmail.com>
2020-07-02 22:08:31 -0400

    made pte weights lowercase

--- package/peptidesim/peptidesim.py
+++ package/peptidesim/peptidesim.py
@@ -694,7 +694,7 @@ class PeptideSim(Configurable):
                  BIASFACTOR={}
                  ... METAD
                  pte-lw: REWEIGHT_BIAS TEMP=@replicas:{{{}}} ARG=METADPT.bias
-                 PRINT FILE=WEIGHTS.dat ARG=pte-lw,ene STRIDE=250
+                 PRINT FILE=weights.dat ARG=pte-lw,ene STRIDE=250
                         '''.format(sigma, hill_height, temps, hills_file_location, bias_factor, temps))
                 break
             else:
