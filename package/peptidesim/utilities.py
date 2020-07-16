eb80e93b8137955e3b6670c29716b880e75719c3
Andrew White <white.d.andrew@gmail.com>
2020-07-15 23:37:52 -0400

    First draft of ramachandran reweighted plots

--- package/peptidesim/utilities.py
+++ package/peptidesim/utilities.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import signal
 import functools
 import os
@@ -147,12 +149,10 @@ def plot_couplings(eds_filename, output_plot='couplings.png', weights_file=None)
     plt.savefig(output_plot, dpi=300)
 
 
-def plot_wte_overlap(ps, production_sim_name, output_plot='wte_overlap.png'):
+def plot_pte_overlap(ps, production_sim_name, output_plot='wte_overlap.png'):
     '''
     This will show how much overlap there is between the PTE bias and the production simulation
     '''
-    import numpy as np
-    import matplotlib.pyplot as plt
     # sum bias from pte step
     hills_file_loc = os.path.join(ps.dir_name, 'pte_hills', 'HILLS_PTE.0')
     bias_file_loc = os.path.join(ps.dir_name, 'pte_hills', 'BIAS.0')
@@ -340,11 +340,13 @@ def prepare_cs_data(ps, shift_dict=None, pte_reweight=False):
     return {'data_dir': data_dir, 'shift_dict': shift_dict, 'plumed': plumed_script, 'cs2_names': cs2_avg_names, 'cs2_values': cs2_values}
 
 
+def load_pte_weights(sim):
+    weights_filename = sim.get_file('weights.dat')
+    return np.loadtxt(weights_filename)
+
 def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
     '''Will plot chemical shift and its average over time across multiple simulations. Can also reweight
     '''
-    import matplotlib.pyplot as plt
-    import numpy as np
     from tqdm import tqdm
 
     if type(sim_list) != list:
@@ -352,16 +354,7 @@ def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
     sim_data = []
     weights = []
     for sim in sim_list:
-        if 'multi-dirs' in sim.metadata:
-            cs_filename = os.path.join(sim.location, sim.metadata['multi-dirs'][0], 'cs_shifts.0.dat')
-            weights_filename = os.path.join(sim.location, sim.metadata['multi-dirs'][0], 'weights.0.dat')
-        else:
-            cs_filename = os.path.join(sim.location, 'cs_shifts.dat')
-            weights_filename = os.path.join(sim.location, 'weights.dat')
-        if not os.path.exists(cs_filename):
-            raise FileNotFoundError('Could not find cs_shifts dat file in {}'.format(cs_filename))
-        if use_weights and not os.path.exists(weights_filename):
-            raise FileNotFoundError('Could not find weights dat file in {}'.format(weights_filename))
+        cs_filename = sim.get_file('cs_shifts.dat')
         with open(cs_filename, 'r') as f:
             header = f.readline().split()[2:]
         # get target values using kind of hack
@@ -376,7 +369,7 @@ def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
         sim_data.append(data)
         # load weights
         if use_weights:
-            wdata = np.loadtxt(weights_filename)
+            wdata = load_pte_weights(sim)
             # drop zeroth row
             weights.append(wdata[1:, 1])
         else:
@@ -389,7 +382,7 @@ def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
             sn = n.split('all-avg-')[-1]
             shift_names.add(sn)
 
-    fig, ax = plt.subplots(nrows=len(shift_names) // 2, ncols=2, figsize=(12, 8))
+    fig, ax = plt.subplots(nrows=len(shift_names) // 2, ncols=2, figsize=(6, 4), sharex=True)
     index = 0
     shift_names = list(shift_names)
     shift_names.sort()
@@ -402,7 +395,7 @@ def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
             for k, data in enumerate(sim_data):
                 if k > 0:
                     ax[i, j].axvline(last_time, linestyle='--', color='gray')
-                ax[i, j].plot(data.time / 1000 + last_time, data[f'avg-{s}'], color='C0', alpha=0.5, label='instantaneous' if k == 0 else None)
+                ax[i, j].plot(data.time / 1000 + last_time, data[f'avg-{s}'], color='C0', alpha=0.5, label='Instantaneous' if k == 0 else None)
                 # compute weighted running average
                 widx = 0  # data.time.count() // 2
                 time = data.time[widx + 1:] / 1000
@@ -412,17 +405,113 @@ def plot_cs(sim_list, use_weights=True, output_plot='cs.png'):
                 w = np.exp(weights[k] - wmax)
                 for wi in tqdm(range(widx + 1, data.time.count())):
                     wmean.append(np.sum(shifts[widx:wi] * w[widx:wi]) / np.sum(w[widx:wi]))
+                ax[i, j].plot(time + last_time, wmean, color='C0', alpha=1.0, label='Run. Avg.' if k == 0 else None)
                 if use_weights:
                     ax[i, j].plot(data.time[1:] / 1000 + last_time,
                                   [np.mean(shifts[:i]) for i in range(1, data.time.count())],
-                                  color='C2', alpha=1.0, label='unweighted running average' if k == 0 else None)
-                ax[i, j].plot(time + last_time, wmean, color='C0', alpha=1.0, label='running average' if k == 0 else None)
-                ax[i, j].plot(data.time / 1000 + last_time, data[targets[s]], color='C1', alpha=1.0, label='target' if k == 0 else None)
+                                  color='C2', alpha=1.0, label='Unw. Run. Avg.' if k == 0 else None, linestyle='--')
+                ax[i, j].plot(data.time / 1000 + last_time, data[targets[s]], color='C1', alpha=1.0, label='Experiment' if k == 0 else None)
                 last_time = data.time.max() / 1000
-            ax[i, j].legend()
             ax[i, j].set_title(pretty_cslabel(s))
             ax[i, j].set_xlabel('Time [ns]')
             ax[i, j].set_ylabel('Shift [ppm]')
+            if i == 0 and j == 1:
+                ax[i,j].legend(loc='upper left', bbox_to_anchor=(1.05, 1))
             index += 1
     plt.tight_layout()
     plt.savefig(output_plot, dpi=300)
+
+def plot_pte_ramachandran(ps, sim, temperature, pte_stride=250, traj_stride=1000, units='kcal/mol', output_name='ramachandran_data'):
+    
+    import matplotlib as mpl
+
+    mpl.rc('text', usetex=True)
+    
+
+    # line-up weights with trajectory steps
+    lcm = np.lcm(pte_stride, traj_stride)
+    pte_every = lcm // pte_stride
+    traj_every = lcm // traj_stride
+    print(f'Based on strides, will only compute Ramachandran at steps {lcm}')
+
+    # set energy
+    plumed_script = f'UNITS ENERGY={units}\n'
+
+    # make pdb for plumed
+    data_dir = os.path.join(ps.dir_name, output_name)
+    os.makedirs(data_dir, exist_ok=True)
+    template_pdb = os.path.join(data_dir, 'template.pdb')
+    atom_number = pdb_for_plumed(input_file=ps.pdb_file, output_file=template_pdb)
+    plumed_script += f'MOLINFO STRUCTURE={template_pdb}\n'
+
+    # get PTE info
+    weights_filename = sim.get_file('weights.dat')
+    # I'm pretty sure I did this correctly, but I cannot get plumed to believe me
+    plumed_script += f'pte-lw: READ FILE={weights_filename} VALUES=pte-lw STRIDE={traj_every} EVERY={pte_every} IGNORE_TIME\n'
+    
+    # make plumed script
+    names =[]
+    for i, s in enumerate(ps.sequences):
+        index = 0
+        for j in range(ps.counts[i]):
+            for k in range(len(s)):
+                if k == 0 or k == len(s) - 1:
+                    continue
+                n = f'{i}-{k+1}-{s[k]}'
+                names.append(n)
+                output_grid = os.path.join(data_dir, f'fes-{n}.dat')
+                plumed_script += f'phi-{n}: TORSION ATOMS=@phi-{ascii_uppercase[index]}{k+1}\n'\
+                                  f'psi-{n}: TORSION ATOMS=@psi-{ascii_uppercase[index]}{k+1}\n'\
+                                  f'HISTOGRAM ...\n'\
+                                  f'ARG=phi-{n},psi-{n}\n'\
+                                  f'GRID_MIN=-3.14,-3.14\n'\
+                                  f'GRID_MAX=3.14,3.14\n'\
+                                  f'GRID_BIN=200,200\n'\
+                                  f'BANDWIDTH=0.05,0.05\n'\
+                                  f'LABEL=hrr-{n}\n'\
+                                  f'STRIDE={traj_every}\n'\
+                                  f'... HISTOGRAM\n'\
+                                  f'fes-{n}: '\
+                                  f'CONVERT_TO_FES GRID=hrr-{n} TEMP={temperature}\n'\
+                                  f'DUMPGRID GRID=fes-{n} FILE={output_grid}\n'
+            index += 1
+
+    plumed_file = os.path.join(data_dir, 'compute_ramachandran.dat')
+    with open(plumed_file, 'w') as f:
+        f.write(plumed_script)
+    if 'multi-dirs' in sim.metadata:
+        trj = os.path.join(sim.location, 
+                            sim.metadata['multi-dirs'][0], 
+                            sim.metadata['traj'])
+    else:
+        path = os.path.join(sim.location, sim.metadata['traj'])
+
+    if True:
+       result = subprocess.call(f'plumed driver --mf_trr {trj} '
+                                f'--plumed {plumed_file}',
+                                shell=True)
+       if result != 0:
+           raise ValueError('Failed to run plumed_driver')
+
+    # now we plot
+    for n in names:
+        data  = np.loadtxt(os.path.join(data_dir, f'fes-{n}.dat'))
+        imdata = data[:,2].reshape(200,200)
+        plt.figure(figsize=(4,3))
+        #plt.title(n)
+        # pick reasonable scaling
+        result = np.quantile(imdata[imdata != np.inf], [0.01, 0.99])
+        # set background color
+        plt.gca().set_facecolor(mpl.cm.get_cmap('viridis')(0))
+        # plot
+        plt.imshow(imdata - result[1], cmap='viridis_r', origin='lower', vmin=result[0] - result[1], vmax=0)
+        plt.xticks((50, 100, 150), (r'$-\frac{\pi}{2}$', '0', r'$\frac{\pi}{2}$'))
+        plt.yticks((50, 100, 150), (r'$-\frac{\pi}{2}$', '0', r'$\frac{\pi}{2}$'))
+        plt.xlabel('$\phi$ [Rad]')
+        plt.ylabel('$\psi$ [Rad]')
+        cbar = plt.colorbar()
+        cbar.ax.set_ylim(0, result[0] - result[1])
+        cbar.ax.set_ylabel(f'$\Delta$ A[{units}]')
+        plt.tight_layout()
+        plt.savefig(n + '.png', dpi=300)
+            
